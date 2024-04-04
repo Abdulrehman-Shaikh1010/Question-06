41ba8dcccc233ad647363c93b35a7920232afbf2
Abdulrehman <you@example.com>
2024-04-04 20:38:35 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//const myName: string = "  Abdulrehman  ";
+//const myName: string = "\tAbdulrehman"
+var myName = "\nAbdulrehman";
+console.log(myName);
+//console.log(myName.trim());
---
+++ main.ts
@@ -0,0 +1,10 @@
+
+//const myName: string = "  Abdulrehman  ";
+
+//const myName: string = "\tAbdulrehman"
+
+const myName: string = "\nAbdulrehman"
+
+console.log(myName);
+
+//console.log(myName.trim());
