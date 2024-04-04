c9133869b7a42f8811e38aff0315d1ce27363909
Abdulrehman <you@example.com>
2024-04-04 20:42:22 +0500

    first commit

--- main.ts
+++ main.ts
@@ -8,3 +8,18 @@ const myName: string = "\nAbdulrehman"
 console.log(myName);
 
 //console.log(myName.trim());
+
+
+
+
+// echo "# Question-06" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Abdulrehman-Shaikh1010/Question-06.git
+// git push -u origin main
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/Abdulrehman-Shaikh1010/Question-06.git
+// git branch -M main
+// git push -u origin main
